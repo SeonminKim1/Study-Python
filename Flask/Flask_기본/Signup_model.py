d70efb0057e7efe725e41bdccfe538c13199a35b
SeonminKim1 <yubi5050@naver.com>
2021-01-10 04:34:16 +0900

    회원가입 py

---
+++ Flask/Flask_기본/Signup_model.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# db에 대한 최상위 변수
+db = SQLAlchemy()
+
+class Fcuser(db.Model):
+    __tablename__ = 'fcuser'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(64))
+    userid = db.Column(db.String(32))
+    username = db.Column(db.String(8))
+
