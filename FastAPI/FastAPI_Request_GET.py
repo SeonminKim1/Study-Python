ffc7a073b88283f1856e0870cb3d41edc798dc2e
SeonminKim1 <yubi5050@naver.com>
2021-05-30 04:11:40 +0900

    GET, POST

--- FastAPI/FastAPI_Request_GET.py
+++ FastAPI/FastAPI_Request_GET.py
@@ -10,7 +10,7 @@ async def read_root():
     return {"Hello": "World"}
 
 # case 1
-# curl http://127.0.0.1/inference/4-5
+# curl http://127.0.0.1:8000/inference/4-5
 @app.get("/inference/{job_id}-{dok_id}")
 async def start_inference_with_docker(job_id: int,
                                       dok_id :int):
