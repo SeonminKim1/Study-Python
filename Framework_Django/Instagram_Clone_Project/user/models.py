440381412e94ef09bb60f420e4f90042dbe0549d
SeonminKim1 <yubi5050@naver.com>
2022-06-11 17:59:33 +0900

    Remove project

--- Framework_Django/Instagram_Clone_Project/user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    user_id = models.TextField()
-    name = models.TextField()
-    nickname = models.TextField()
-    profile_image = models.TextField()
