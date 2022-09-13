0a2208b86d1ef2114b22f42f0295901b618bbce7
SeonminKim1 <yubi5050@naver.com>
2022-09-13 12:29:55 +0900

    Add python Generator

---
+++ Skills_Generator/ex_generator1.py
@@ -0,0 +1,11 @@
+#제네레이터 생성
+def ex_func1(nums):
+    for i in nums:
+        yield i * i
+
+if __name__ =="__main__":
+    generator_ex = ex_func1([1,2,3])
+    print(next(generator_ex)) # 1
+    print(next(generator_ex)) # 4
+    print(next(generator_ex)) # 9
+    print(next(generator_ex)) # 더 반환할 값이 없으면 StopIteration 발생
